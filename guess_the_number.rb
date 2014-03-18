17f801ab04d518b6994702e7d4cc5fd200c99601
Nreynoso20 <nathan_reynoso@yahoo.com>
2014-03-18 18:05:22 +0000

    checked guess secret number and returned a message

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,3 +1,11 @@
 puts "Welcome to Guess-the-Number!"
 secret_number = rand(80)
-puts secret_number
+
+puts "Guess a number between 0-79:"
+guess = gets.chomp.to_i
+
+if secret_number == guess
+  puts "Wow good job you picked the right number!"
+else
+  puts "Nice try but you didnt pick the right number. The secret number was #{secret_number}."
+end
