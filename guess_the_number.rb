cb20e6d8620c76b54c8a75abbf09e90cfc1882c1
Nreynoso20 <nathan_reynoso@yahoo.com>
2014-03-19 17:59:30 +0000

    We changed the if statement to until block

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,11 +1,14 @@
 puts "Welcome to Guess-the-Number!"
-secret_number = rand(80)
+secret_number = rand(20)
 
-puts "Guess a number between 0-79:"
+puts "Guess a number between 0-20:"
 guess = gets.chomp.to_i
 
-if secret_number == guess
-  puts "Wow good job you picked the right number!"
-else
-  puts "Nice try but you didnt pick the right number. The secret number was #{secret_number}."
+until guess == secret_number
+  puts "Nice try but you didnt pick the right number."
+  guess = gets.chomp.to_i 
 end
+
+puts "Good job you got the right number! Try again to see if your a boss."
+
+
